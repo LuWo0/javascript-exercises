a64ffdcc705a271e30cad393a626395adac9e672
LuWo0 <loviedo46@yahoo.com>
2022-07-04 21:20:05 -0700

    Finished ex.07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  // ([°F] − 32) × 5⁄9
+  let cel =((temp - 32) * (5/9));
+  if (cel === 0 ) return cel;
 
+  return Number(cel.toFixed(1));
+  
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  // [°C] × 9⁄5 + 32
+  let fah = (temp * (9/5)) + 32; 
+  if (fah === 0 ) return fah;
 
+  return Number(fah.toFixed(1));
 };
 
 // Do not edit below this line
