0fe2f92b2ad928e4077e8bfc65b77ce4b2afef20
LuWo0 <loviedo46@yahoo.com>
2022-07-04 20:40:16 -0700

    Finished ex.4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...nums) {
+    //if (NaN(nums)) {return arr;}
+    nums.forEach(num => {
+        let index = arr.indexOf(num);
+        if (index > -1) arr.splice(index, 1);
+    });
+    return arr;
 };
 
 // Do not edit below this line
