bd0d4173b582b20ffb6f4f5278c3c50aae0d046f
LuWo0 <loviedo46@yahoo.com>
2022-07-04 20:17:31 -0700

    Finished ex.3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const splitStr = str.split("");
+    const reverseStr = splitStr.reverse();
+    const newStr = reverseStr.join("");
+    return newStr;
 };
 
 // Do not edit below this line
