5faf31d9cac2b4aea6401283f22c00fdc8fa30dc
LuWo0 <loviedo46@yahoo.com>
2022-07-04 20:57:43 -0700

    Finished ex.05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,4 @@
 const removeFromArray = function(arr, ...nums) {
-    //if (NaN(nums)) {return arr;}
     nums.forEach(num => {
         let index = arr.indexOf(num);
         if (index > -1) arr.splice(index, 1);
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
 
+    if(num1 < 0 || num2 < 0) return "ERROR";
+
+    let sum = 0;
+
+    if (num1 < num2){
+        while(num1 <= num2) {
+            sum += num1;
+            num1++;
+        }
+    }
+    else{ 
+        while(num1 >= num2) {
+            sum += num2;
+            num2++;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
