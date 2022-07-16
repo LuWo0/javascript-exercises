651e02399cefe09cc1467ec2c49a88862090adef
LuWo0 <loviedo46@yahoo.com>
2022-07-16 01:46:07 -0700

    finished ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function isPalindrome(str){
+    const newStr = str.replace(/[^\w]/gi, '').toLowerCase();
+    console.log(newStr);
+    let left = 0;
+    let right = newStr.length - 1;
+    while (left < right) {
+        if (newStr[left] !== newStr[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
