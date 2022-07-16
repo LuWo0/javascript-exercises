7c335032ba6a0dd97f16cd83c5f0be85cafd1650
LuWo0 <loviedo46@yahoo.com>
2022-07-16 03:06:44 -0700

    Finished ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = people => {
+    let total = 0;
+    let oldestPerson = { };
+    let todaysDate = new Date().getFullYear();
 
-};
+    people.reduce((a, b) => {
+        if (!a.hasOwnProperty("yearOfDeath")) a["yearOfDeath"] = todaysDate;
+        if ((a.yearOfDeath - a.yearOfBirth) > total) {
+            total = (a.yearOfDeath - a.yearOfBirth);
+            oldestPerson = a;
+        }
+        return b;
+    });
+    console.log(oldestPerson);
+    return oldestPerson;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
