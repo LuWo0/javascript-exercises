48f174effe8ef7c7243a91b76710269a20d5044b
LuWo0 <loviedo46@yahoo.com>
2022-07-16 02:12:54 -0700

    Finished ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function fib (index){
+    if (index < 0) return "OOPS";
+    let fibo = parseFloat((Math.pow(1.6180339, index) -  Math.pow(-0.6180339, index)) / 2.236067977);
+    let rounded = Math.round(fibo);
+    console.log(rounded);
+    return rounded;
 };
 
 // Do not edit below this line
